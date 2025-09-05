05c57dafb110650e63bc53440315a6184362e368
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2025-09-05 22:06:43 +0200

    Add unit test for collinear points

--- src/geom/point/check.rs
+++ src/geom/point/check.rs
@@ -6,12 +6,12 @@ pub fn are_points_coplanar(pts: Vec<Point>) -> bool {
     if pts.len() <= 3 {
         return true;
     }
-
+    // TODO: Finish. It will require a dot product of 2 vectors.
     true
 }
 
 /// Checks if (multiple) points are collinear
-pub fn are_points_collinear(pts: Vec<Point>) -> bool {
+pub fn are_points_collinear(pts: &[Point]) -> bool {
     if pts.len() <= 2 {
         return true; // 1 or 2 points are always collinear
     }
@@ -25,4 +25,24 @@ pub fn are_points_collinear(pts: Vec<Point>) -> bool {
     }
     // Unit vectors must be equal if points are collinear
     are_vectors_close(&unit_vectors)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_are_points_collinear() {
+        // Collinear
+        let p0 = Point::new(0., 0., 0.);
+        let p1 = Point::new(1., 0., 0.);
+        let p2 = Point::new(2., 0., 0.);
+        assert!(are_points_collinear(&[p0]));
+        assert!(are_points_collinear(&[p0, p1]));
+        assert!(are_points_collinear(&[p0, p1, p2]));
+        // Not collinear
+        let p3 = Point::new(1., 1., 0.);
+        assert!(!are_points_collinear(&[p0, p1, p2, p3]));
+    }
 }
