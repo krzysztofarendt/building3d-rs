15c239e175d8ca846f4b5986f16d581f30a346b1
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2025-09-06 11:36:26 +0200

    Add Point::many_new_between_2_points()

--- src/geom/point.rs
+++ src/geom/point.rs
@@ -41,6 +41,27 @@ impl Point {
         let new_v = v_pt1 * (1. - alpha_v) + v_pt2 * alpha_v;
         Self::new(new_v.dx, new_v.dy, new_v.dz)
     }
+
+    /// Creates `num` new points along the edge spanning from `pt1` to `pt2`.
+    ///
+    /// Points are uniformly distributed based on `num`.
+    /// Returns a vector of all points: `[pt1, new_1, new_2, ..., new_num, pt2]`
+    pub fn many_new_between_2_points(pt1: Self, pt2: Self, num: usize) -> Vec<Point> {
+        let mut vecs: Vec<Point> = vec![pt1];
+        let vp1 = Vector::from_a_point(pt1);
+        let vp2 = Vector::from_a_point(pt2);
+
+        for i in 1..num + 1 {
+            let alpha = (i as f64) / (num + 1) as f64;
+            let alpha_v = Vector::new(alpha, alpha, alpha);
+            let new_vec = vp1 * (1. - alpha_v) + vp2 * alpha_v;
+            let new_pt = Point::new(new_vec.dx, new_vec.dy, new_vec.dz);
+            vecs.push(new_pt);
+        }
+        vecs.push(pt2);
+
+        vecs
+    }
 }
 
 impl fmt::Display for Point {
@@ -106,5 +127,24 @@ mod tests {
         assert!(ptest.is_close(&p0));
         let ptest = Point::new_between_2_points(p0, p0, 1.0);
         assert!(ptest.is_close(&p0));
+    }
+
+    #[test]
+    fn test_many_new_between_2_points() {
+        let p_beg = Point::new(0., 0., 0.);
+        let p_end = Point::new(1., 1., 1.);
+        let num: usize = 3;
+        let pt_all = Point::many_new_between_2_points(p_beg, p_end, num);
+        let expected = vec![
+            p_beg,
+            Point::new(0.25, 0.25, 0.25),
+            Point::new(0.5, 0.5, 0.5),
+            Point::new(0.75, 0.75, 0.75),
+            p_end,
+        ];
+        // Check if output points are same as expected
+        for (pa, pb) in pt_all.iter().zip(expected) {
+            assert!(pa.is_close(&pb));
+        }
     }
 }
