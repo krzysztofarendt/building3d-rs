832cd660eba68091decc536b557d93497ddbb676
Krzysztof Arendt <16005748+krzysztofarendt@users.noreply.github.com>
2025-09-30 07:29:17 +0200

    Add a complex shape to floor plan example

--- examples/floor_plan.rs
+++ examples/floor_plan.rs
@@ -6,7 +6,16 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
 
+    let plan: Vec<(f64, f64)> = vec![
+        (0., 0.), (5., 0.), (5., 2.), (3., 2.), (3., 7.), (0., 5.)
+    ];
+    let height: f64 = 1.8;
+    let name = Some("building".to_string());
+
     let fp = FloorPlan {
+        plan,
+        height,
+        name,
         ..Default::default()
     };
     let sld = Solid::from_floor_plan(fp)?;
